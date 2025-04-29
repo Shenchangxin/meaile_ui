ab55b6daa1a9648c6808bf0d7c8e2c19e1391d4a
Shenchangxin <2863727114@qq.com>
2025-04-29 23:08:27 +0800

    配置路由

--- lib/application.dart
+++ lib/application.dart
@@ -1,15 +1,18 @@
+import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 import 'package:meaile_ui/pages/home/index.dart';
 import 'package:meaile_ui/pages/login.dart';
+import 'package:meaile_ui/routes.dart';
 
 class Application extends StatelessWidget {
   const Application({super.key});
 
   @override
   Widget build(BuildContext context) {
-
+    final router = FluroRouter();
+    Routes.configureRoutes(router);
     return MaterialApp(
-      home: LoginPage(),
+      onGenerateRoute: router.generator,
     );
   }
 }
